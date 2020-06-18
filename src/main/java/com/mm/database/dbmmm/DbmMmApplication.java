27281e2177b786a23e4faab3bcd310574390cd6f
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-17 22:54:21 -0600

    Project base

---
+++ src/main/java/com/mm/database/dbmmm/DbmMmApplication.java
@@ -0,0 +1,13 @@
+package com.mm.database.dbmmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmMmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmMmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mm/database/dbmmm/DbmMmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.database.dbmmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmMmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
