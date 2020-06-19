60c8d0601aeb5cda3d577083be1aaf6c33576408
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-18 20:10:52 -0600

    Implementing findById, findByName, deleteById, insert and update JdbcTemplate methods

--- src/main/java/com/mm/database/dbmmm/helpers/InitCommandLine.java
+++ src/main/java/com/mm/database/dbmmm/helpers/InitCommandLine.java
@@ -1,11 +1,14 @@
 package com.mm.database.dbmmm.helpers;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.mm.database.dbmmm.entity.Person;
 import com.mm.database.dbmmm.jdbc.PersonJdbcDao;
 
 @Component
@@ -18,7 +21,17 @@ public class InitCommandLine implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("All users -> {}", dao.findAll());
+		Thread.sleep(5000);
+		logger.info("*********************findById 1001**************************");
+		logger.info("User -> {}", dao.findById(1001));
+		logger.info("*********************findByName 1001**************************");
+		logger.info("All User -> {}", dao.findByName("Nelson Miranda"));
+		logger.info("*********************deleteById 1002**************************");
+		logger.info("Rows affected -> {}", dao.deleteById(1002));
+		logger.info("*********************insert 1004**************************");
+		logger.info("Rows affected -> {}", dao.insert(new Person(1004, "Rodolfo Ramos", "San Salvador", new Date())));
+		logger.info("*********************update 1003**************************");
+		logger.info("Rows affected -> {}", dao.update(new Person(1003, "Ricardo Miranda", "La Libertad", new Date())));
 	}
    
 }
--- src/main/java/com/mm/database/dbmmm/jdbc/PersonJdbcDao.java
+++ src/main/java/com/mm/database/dbmmm/jdbc/PersonJdbcDao.java
@@ -1,5 +1,6 @@
 package com.mm.database.dbmmm.jdbc;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,38 @@ public class PersonJdbcDao {
 		return jdbcTemplate.query("select * from person", 
 				new BeanPropertyRowMapper<Person>(Person.class));
 	}
+	
+	//select * from person where id=?
+	public Person findById(int id) {
+		return jdbcTemplate.queryForObject("select * from person where id=?", 
+				new Object[] {id}, new BeanPropertyRowMapper<Person>(Person.class));
+	}
+	
+	//select * from person where name=?
+	public List<Person> findByName(String name){
+		return jdbcTemplate.query("select * from person where name = ?",
+				new Object[] {name},new BeanPropertyRowMapper<Person>(Person.class));
+	}
+	
+	//delete from person where id=?
+	public int deleteById(int id) {
+		return jdbcTemplate.update("delete from person where id=?", new Object[] {id});
+	}
+	
+	public int insert(Person person) {
+		return jdbcTemplate.update("insert into person(id,name,location,birthdate) "
+				+ "values (?,?,?,?);", 
+				new Object[] {person.getId(), person.getName(),
+						person.getLocation(), new Timestamp(person.getBirthDate().getTime())});
+	}
+	
+	public int update(Person person) {
+		return jdbcTemplate.update("update person "
+				+ "set name = ?, location=?, birthdate=? "
+				+ "where id=?", 
+				new Object[] {person.getName(), person.getLocation(),
+						new Timestamp(person.getBirthDate().getTime()),
+						person.getId()});
+	}
+	
 }
